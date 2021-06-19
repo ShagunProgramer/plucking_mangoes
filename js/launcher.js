8f690c7c4b0d79bb4b44f8e88ded99316b2dc8f1
ShagunProgramer <77391458+ShagunProgramer@users.noreply.github.com>
2021-06-19 15:12:12 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,44 @@
+class launcher {
+    constructor(bodyA, pB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+       fly(){
+
+this.launcher.bodyA=null
+
+
+
+       }
+       attach(body){
+
+this.launcher.bodyA=body
+
+
+       }
+       
+    display(){
+        if (this.launcher.bodyA){
+
+
+            var pointA = this.launcher.bodyA.position;
+            var pointB = this.launcher.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y); 
+
+
+
+
+
+
+        }
+      
+    }
+    
+}
